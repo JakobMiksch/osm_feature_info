cb2a93a253f29e5e178051a90fbbfae759dfbd20
iboates <iboates@gmail.com>
2024-03-28 11:35:54 +0100

    Update run.sh
    Co-authored-by: Jakob Miksch <info@jakobmiksch.eu>

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ PBF_FILE=${1}
 docker compose down && docker compose up -d
 
 # download sample data
-wget -O data/sample.pbf $PBF_FILE
+wget -O data/sample.pbf ${PBF_FILE}
 
 # add function to DB
 docker compose exec db psql -f /data/query_function.sql
